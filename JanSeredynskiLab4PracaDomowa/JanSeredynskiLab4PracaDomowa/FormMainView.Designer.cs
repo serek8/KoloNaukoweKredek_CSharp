0f825096acccac39b4d64a08df5e1831139635e7
serek8 <serek8@gmail.com>
2015-05-20 03:18:17 -0700

    Laboratorium4PracaDomowa- Gotowe

---
+++ JanSeredynskiLab4PracaDomowa/JanSeredynskiLab4PracaDomowa/FormMainView.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace JanSeredynskiLab4PracaDomowa
+{
+    partial class FormMainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvView = new System.Windows.Forms.DataGridView();
+            this.bTest1 = new System.Windows.Forms.Button();
+            this.bTest2 = new System.Windows.Forms.Button();
+            this.bTest3 = new System.Windows.Forms.Button();
+            this.bTest4 = new System.Windows.Forms.Button();
+            this.bTest5 = new System.Windows.Forms.Button();
+            this.bTest6 = new System.Windows.Forms.Button();
+            this.bTest7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvView
+            // 
+            this.DgvView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DgvView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvView.Location = new System.Drawing.Point(12, 12);
+            this.DgvView.Name = "DgvView";
+            this.DgvView.Size = new System.Drawing.Size(560, 269);
+            this.DgvView.TabIndex = 0;
+            // 
+            // bTest1
+            // 
+            this.bTest1.Location = new System.Drawing.Point(12, 287);
+            this.bTest1.Name = "bTest1";
+            this.bTest1.Size = new System.Drawing.Size(75, 23);
+            this.bTest1.TabIndex = 1;
+            this.bTest1.Text = "bTest1";
+            this.bTest1.UseVisualStyleBackColor = true;
+            this.bTest1.Click += new System.EventHandler(this.bTest1_Click);
+            // 
+            // bTest2
+            // 
+            this.bTest2.Location = new System.Drawing.Point(93, 287);
+            this.bTest2.Name = "bTest2";
+            this.bTest2.Size = new System.Drawing.Size(75, 23);
+            this.bTest2.TabIndex = 2;
+            this.bTest2.Text = "bTest2";
+            this.bTest2.UseVisualStyleBackColor = true;
+            this.bTest2.Click += new System.EventHandler(this.bTest2_Click);
+            // 
+            // bTest3
+            // 
+            this.bTest3.Location = new System.Drawing.Point(174, 287);
+            this.bTest3.Name = "bTest3";
+            this.bTest3.Size = new System.Drawing.Size(75, 23);
+            this.bTest3.TabIndex = 3;
+            this.bTest3.Text = "bTest3";
+            this.bTest3.UseVisualStyleBackColor = true;
+            this.bTest3.Click += new System.EventHandler(this.bTest3_Click);
+            // 
+            // bTest4
+            // 
+            this.bTest4.Location = new System.Drawing.Point(255, 287);
+            this.bTest4.Name = "bTest4";
+            this.bTest4.Size = new System.Drawing.Size(75, 23);
+            this.bTest4.TabIndex = 4;
+            this.bTest4.Text = "bTest4";
+            this.bTest4.UseVisualStyleBackColor = true;
+            this.bTest4.Click += new System.EventHandler(this.bTest4_Click);
+            // 
+            // bTest5
+            // 
+            this.bTest5.Location = new System.Drawing.Point(336, 287);
+            this.bTest5.Name = "bTest5";
+            this.bTest5.Size = new System.Drawing.Size(75, 23);
+            this.bTest5.TabIndex = 5;
+            this.bTest5.Text = "bTest5";
+            this.bTest5.UseVisualStyleBackColor = true;
+            this.bTest5.Click += new System.EventHandler(this.bTest5_Click);
+            // 
+            // bTest6
+            // 
+            this.bTest6.Location = new System.Drawing.Point(417, 287);
+            this.bTest6.Name = "bTest6";
+            this.bTest6.Size = new System.Drawing.Size(75, 23);
+            this.bTest6.TabIndex = 6;
+            this.bTest6.Text = "bTest6";
+            this.bTest6.UseVisualStyleBackColor = true;
+            this.bTest6.Click += new System.EventHandler(this.bTest6_Click);
+            // 
+            // bTest7
+            // 
+            this.bTest7.Location = new System.Drawing.Point(498, 287);
+            this.bTest7.Name = "bTest7";
+            this.bTest7.Size = new System.Drawing.Size(75, 23);
+            this.bTest7.TabIndex = 7;
+            this.bTest7.Text = "bTest7";
+            this.bTest7.UseVisualStyleBackColor = true;
+            this.bTest7.Click += new System.EventHandler(this.bTest7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 322);
+            this.Controls.Add(this.bTest7);
+            this.Controls.Add(this.bTest6);
+            this.Controls.Add(this.bTest5);
+            this.Controls.Add(this.bTest4);
+            this.Controls.Add(this.bTest3);
+            this.Controls.Add(this.bTest2);
+            this.Controls.Add(this.bTest1);
+            this.Controls.Add(this.DgvView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvView;
+        private System.Windows.Forms.Button bTest1;
+        private System.Windows.Forms.Button bTest2;
+        private System.Windows.Forms.Button bTest3;
+        private System.Windows.Forms.Button bTest4;
+        private System.Windows.Forms.Button bTest5;
+        private System.Windows.Forms.Button bTest6;
+        private System.Windows.Forms.Button bTest7;
+    }
+}
+
---
+++ JanSeredynskiLab4PracaDomowa/JanSeredynskiLab4PracaDomowa/FormMainView.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JanSeredynskiLab4PracaDomowa
+{
+    public partial class FormMainView : Form
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext();
+        public FormMainView()
+        {
+            InitializeComponent();
+            dc = new DataClasses1DataContext();
+        }
+
+        private void bTest1_Click(object sender, EventArgs e)
+        {
+            var query = from c in dc.Products select c;
+            DgvView.DataSource = query;
+        }
+
+        private void bTest2_Click(object sender, EventArgs e)
+        {
+            var query = from c in dc.Products select new { c.ProductID, c.ProductName };
+            DgvView.DataSource = query;
+        }
+
+        private void bTest3_Click(object sender, EventArgs e)
+        {
+            var query = from c in dc.Products orderby c.ProductID descending select new { c.ProductID, c.ProductName };
+            DgvView.DataSource = query;
+        }
+
+        private void bTest4_Click(object sender, EventArgs e)
+        {
+            var query = from c in dc.Products
+                        where c.ProductID < 10
+                        where c.ProductName.Substring(0,1) == "C"
+                        orderby c.ProductID
+                        select new { c.ProductID, c.ProductName };
+            DgvView.DataSource = query;
+        }
+
+        private void bTest5_Click(object sender, EventArgs e)
+        {
+            var query = from c in dc.Products
+                        orderby c.ProductID
+                        select new { c.ProductID, c.ProductName, c.Category.CategoryName }; // odwolujemy sie do Category jak do siebie bo mamy Relations
+            DgvView.DataSource = query; 
+        }
+
+        private void bTest6_Click(object sender, EventArgs e)
+        {
+            var query = (from c in dc.Products
+                         orderby c.ProductName
+                         select new { c.ProductID, c.ProductName, c.Category.CategoryName }).ToArray();
+            MessageBox.Show(query[0].ToString());
+        }
+
+        private void bTest7_Click(object sender, EventArgs e)
+        {
+            var query = (from c in dc.Products
+                         select c).Count();
+            MessageBox.Show(query.ToString());
+        }
+    }
+}
