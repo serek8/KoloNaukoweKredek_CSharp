e3c7842592ad2509e25cbf9623f513df933c0ef5
serek8 <serek8@gmail.com>
2015-05-27 22:40:45 +0200

    Zadanie z laboratorium

---
+++ JanSeredynskiDawidKobierskiLab5Zad0/JanSeredynskiLabX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JanSeredynskiLabX.MvcApplication" Language="C#" %>
---
+++ JanSeredynskiDawidKobierskiLab5Zad0/JanSeredynskiLabX/Models/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JanSeredynskiLabX.Models
+{
+    public class LibraryDbContext: DbContext
+    {
+        public LibraryDbContext() : base("DefaultContext") { }
+        public virtual DbSet<Movie> Movies{set; get;}
+    }
+}
